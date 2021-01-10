b7a647ad2b4b54070ac5dc1535a264e8cf364e12
passionbull <wnsl5684@gmail.com>
2021-01-11 00:24:53 +0900

    no use steemlogin

--- src/client/steemConnectAPI.js
+++ src/client/steemConnectAPI.js
@@ -1,9 +1,8 @@
-import sc2 from 'steemlogin';
+import sc2 from 'sc2-sdk';
 
 const api = sc2.Initialize({
   app: process.env.STEEMCONNECT_CLIENT_ID,
   baseURL: process.env.STEEMCONNECT_HOST,
   callbackURL: process.env.STEEMCONNECT_REDIRECT_URL,
 });
-
 export default api;
